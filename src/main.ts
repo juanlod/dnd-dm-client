cf61939035dad122ed83b947dba2b7f09f8660ef
Juan José López Díaz <31276123+juanlod@users.noreply.github.com>
2025-08-15 00:32:13 +0200

    character summary

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,24 @@
-import 'zone.js'; // imprescindible para Angular con Zone
+import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
 import { App } from './app/app';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(App, appConfig).catch(err => console.error(err));
+import { NzConfig, provideNzConfig } from 'ng-zorro-antd/core/config';
+
+const ngZorroConfig: NzConfig = {
+  button: { nzSize: 'small' },
+  tabs:   { nzSize: 'small' },
+  table:  { nzSize: 'small' },
+  form:   { nzNoColon: true }
+};
+
+// Fusiona tu appConfig existente con la config de NG-ZORRO
+const finalConfig: ApplicationConfig = mergeApplicationConfig(appConfig, {
+  providers: [
+    provideNzConfig(ngZorroConfig)
+  ]
+});
+
+bootstrapApplication(App, finalConfig)
+  .catch(err => console.error(err));
